704c001294100d383fe6656955310ca7dfa739f2
Khurram Wasim <khurram0403@gmail.com>
2018-01-05 16:09:36 +0500

    add password attribite to users and added has_secure_method_to_user.rb file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ActiveRecord::Base
   has_many :articles
   before_save { self.email = email.downcase }
-validates :username, presence: true,
+  validates :username, presence: true,
                      uniqueness: { case_sensitive: false },
                      length: { minimum: 3, maximum: 50 }
-validate_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-validates :email, presence: true,
+  validate_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
                   length: { maximum: 50},
                   uniqueness: { case_sensitive: false },
                   format: { with: validate_email_regex}
+  has_secure_password
 end
